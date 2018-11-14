698276173f6a2231d5988592d0d435850dd2351b
Andres Diaz <aidm@outlook.com>
2018-11-14 03:42:28 -0500

    Facturas 100%, OP 40%, OC 40%, Resumen 100%

--- SGL/Controllers/HomeController.cs
+++ SGL/Controllers/HomeController.cs
@@ -1,24 +1,79 @@
 ﻿using SGL.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+
 
 namespace SGL.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index(){ return View(); }
-        public ActionResult Login(){ return View(); }
-        public ActionResult Resumen() { return View(); }
-        public ActionResult Facturas_Listar() { return View(); }
-        public ActionResult Facturas_Subir() { return View(); }
-        public ActionResult OP_Listar() { return View(); }
-        public ActionResult OP_Subir() { return View(); }
-        public ActionResult OC_Generar() { return View(); }
+        public ActionResult Login(){return View();}
+
+        public ActionResult OP_Subir()
+        {
+            // Validacion de ingreso
+            usuario usuario = (usuario)Session["Usuario"];
+            if (usuario == null) return RedirectToAction("Login");
+
+            return View();
+        }
+
+        public ActionResult OC_Generar()
+        {
+            // Validacion de ingreso
+            usuario usuario = (usuario)Session["Usuario"];
+            if (usuario == null) return RedirectToAction("Login");
+
+            return View();
+        }
+
+        public ActionResult Facturas_Subir()
+        {
+            // Validacion de ingreso
+            usuario usuario = (usuario)Session["Usuario"];
+            if (usuario == null) return RedirectToAction("Login");
+
+            return View();
+        }
 
 
+        public ActionResult Eliminar_Factura(factura fac_pas)
+        {
+            // Validacion de ingreso
+            usuario usuario = (usuario)Session["Usuario"];
+            if (usuario == null) return RedirectToAction("Login");
+
+            using (Entities obj = new Entities())
+            {
+                var x = from fac in obj.factura
+                        where fac.codFactura.Equals(fac_pas.codFactura)
+                        select fac;
+
+                factura objetivo = x.ToList().ElementAt(0);
+
+                edicion edit = new edicion();
+                edit.numero = obj.edicion.ToList().Last().numero + 1;
+                edit.codDoc = objetivo.codFactura;
+                edit.codUsuario = usuario.codUsuario;
+                edit.tipoDoc = "factura";
+                edit.modificacion = "eliminacion";
+                edit.fecha = DateTime.Today;
+                edit.hora = DateTime.Now.TimeOfDay;
+                obj.edicion.Add(edit);
+
+                obj.factura.Remove(objetivo);
+                obj.SaveChanges();
+                return RedirectToAction("Facturas_Listar");
+            }
+        }
 
         [HttpPost]
         public ActionResult Login(string usuario, string password)
@@ -32,24 +87,22 @@ namespace SGL.Controllers
                             select usr;
 
                     usuario user = x.ToList().ElementAt(0);
-                    ViewBag.nombre = user.nombres + " " + user.apellidos;
-
                     Session["Usuario"] = user;
 
-                    return View("Resumen");
+                    ViewBag.Response = "Ploblema.";
                 }
+                    return RedirectToAction("Resumen");
             }
             catch (ArgumentOutOfRangeException e)
             {
                 ViewBag.Response = "Usuario o password incorrectos.";
                 return View();
             }
-        }
-
-        [HttpPost]
-        public ActionResult Facturas_Subir(string usuario, string password)
-        {
-            return View("Facturas_Listar");
+            catch (Exception e)
+            {
+                ViewBag.Message = "Tipo de error: " + e.GetType() + "Mensaje: " + e.Message;
+                return View();
+            }
         }
 
 
@@ -57,11 +110,73 @@ namespace SGL.Controllers
         {
             Session["Usuario"] = null;
 
-            return View("Login");
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Resumen() {
+
+            // Validacion de ingreso
+            usuario usuario = (usuario)Session["Usuario"];
+            if (usuario == null) return RedirectToAction("Login");
+
+            using (Entities obj = new Entities())
+            {
+                var x = obj.edicion;
+                List<edicion> lista_ediciones = x.ToList();
+                List<edicion> nueva_lista_ediciones = new List<edicion>();
+
+                if (usuario.tipoUsuario.Equals("agente"))
+                {
+                    foreach (edicion edi in lista_ediciones)
+                    {
+                        if (edi.codUsuario.Equals(usuario.codUsuario)) nueva_lista_ediciones.Add(edi);
+                    }
+                }
+                else nueva_lista_ediciones = lista_ediciones;
+
+                ViewBag.usuario = usuario;
+                ViewBag.lista_ediciones = nueva_lista_ediciones;
+                return View();
+            }
+        }
+
+
+        public ActionResult Facturas_Listar()
+        {
+            // Validacion de ingreso
+            usuario usuario = (usuario)Session["Usuario"];
+            if (usuario == null) return RedirectToAction("Login");
+
+            using (Entities obj = new Entities())
+            {
+                var x = obj.factura;
+                List<factura> lista_facturas = x.ToList();
+                ViewBag.lista_facturas = lista_facturas;
+                return View();
+            }
+        }
+
+        public ActionResult OP_Listar()
+        {
+            // Validacion de ingreso
+            usuario usuario = (usuario)Session["Usuario"];
+            if (usuario == null) return RedirectToAction("Login");
+
+            using (Entities obj = new Entities())
+            {
+                var x = obj.OP;
+                List<OP> lista_OP = x.ToList();
+                ViewBag.lista_OP = lista_OP;
+                return View();
+            }
         }
 
         public ActionResult OC_Listar()
         {
+            // Validacion de ingreso
+            usuario usuario = (usuario)Session["Usuario"];
+            if (usuario == null) return RedirectToAction("Login");
+
             using (Entities obj = new Entities())
             {
                 var x = obj.OC;
@@ -70,5 +185,63 @@ namespace SGL.Controllers
                 return View();
             }
         }
+
+        [HttpPost]
+        public ActionResult Facturas_Subir(HttpPostedFileBase file)
+        {
+            // Validacion de ingreso
+            usuario usuario = (usuario)Session["Usuario"];
+            if (usuario==null) return RedirectToAction("Login");
+
+            try
+            {
+                // Coloca archivo ZIP en directorio del servidor
+                string servidor = Server.MapPath("~/UploadedFiles");
+                string _FileName = Path.GetFileName(file.FileName);
+                string _path = Path.Combine(servidor, _FileName);
+                file.SaveAs(_path);
+
+                // Leer archivo ZIP en UploadedFiles y extra información de los XML
+                Lector lector = new Lector(servidor, _path);
+                List<factura> lista_facturas = lector.paquete_facturas();
+                ViewBag.lista_facturas = lista_facturas;
+
+                //Sube las facturas a la BD
+                lector.subir_facturas();
+                ViewBag.message = "Facturas subidas exitosamente.";
+
+
+                // Guarda edicion de cambios en BD y retorna vista
+                using (Entities obj = new Entities())
+                {
+                    foreach (factura fac in lista_facturas)
+                    {
+                        edicion edit = new edicion();
+                        edit.numero = obj.edicion.ToList().Last().numero + 1;
+                        edit.codDoc = fac.codFactura;
+                        edit.codUsuario = usuario.codUsuario;
+                        edit.tipoDoc = "factura";
+                        edit.modificacion = "creacion";
+                        edit.fecha = DateTime.Today;
+                        edit.hora = DateTime.Now.TimeOfDay;
+                        obj.edicion.Add(edit);
+                        obj.SaveChanges();
+                    }
+                    return View();
+                }
+            }
+            catch (System.Data.Entity.Infrastructure.DbUpdateException e)
+            {
+                ViewBag.Message = "Algunas de las facturas ya han sido subidas al servidor.";
+                return View();
+            }
+            catch (Exception e)
+            {
+                ViewBag.Message = "Tipo de error: "+ e.GetType()+ "Mensaje: "+ e.Message;
+                return View();
+            }
+        }
     }
+
+    
 }
---
+++ SGL/Controllers/Lector.cs
@@ -0,0 +1,101 @@
+﻿using SGL.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Collections;
+
+
+namespace SGL.Controllers
+{
+    public class Lector
+    {
+        string servidor;
+        string zip;
+        List<factura> paquete = new List<factura>();
+
+        public Lector(string servidor, string zip)
+        {
+            this.servidor = servidor;
+            this.zip = zip;
+        }
+
+
+        public List<factura> paquete_facturas()
+        {
+            // Descomprime ZIP
+
+            System.IO.Compression.ZipFile.ExtractToDirectory(zip, servidor);
+
+            // Extrae informacion de Facturas en XML
+
+            paquete = new List<factura>();
+            string[] grupofac = Directory.GetFiles(servidor, "*.xml");
+
+            foreach (string file in grupofac)
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(file);
+
+                string direccion = xmlDoc.DocumentElement.ChildNodes[0].ChildNodes[0].ChildNodes[0].ChildNodes[0].ChildNodes[22].ChildNodes[1].InnerText;
+                string planta = xmlDoc.DocumentElement.ChildNodes[0].ChildNodes[0].ChildNodes[0].ChildNodes[0].ChildNodes[13].ChildNodes[1].InnerText;
+                string destino = xmlDoc.DocumentElement.ChildNodes[0].ChildNodes[0].ChildNodes[0].ChildNodes[0].ChildNodes[53].ChildNodes[1].InnerText;
+                string fecha = xmlDoc.DocumentElement.ChildNodes[0].ChildNodes[0].ChildNodes[0].ChildNodes[0].ChildNodes[10].ChildNodes[1].InnerText;
+                string codfactura = xmlDoc.DocumentElement.ChildNodes[0].ChildNodes[0].ChildNodes[0].ChildNodes[0].ChildNodes[00].ChildNodes[0].InnerText;
+                string galones = xmlDoc.DocumentElement.ChildNodes[15].ChildNodes[1].InnerText;
+                string octanaje = xmlDoc.DocumentElement.ChildNodes[15].ChildNodes[6].ChildNodes[0].InnerText;
+                string monto = xmlDoc.DocumentElement.ChildNodes[0].ChildNodes[0].ChildNodes[0].ChildNodes[0].ChildNodes[41].ChildNodes[1].InnerText;
+                string scop = xmlDoc.DocumentElement.ChildNodes[0].ChildNodes[0].ChildNodes[0].ChildNodes[0].ChildNodes[38].ChildNodes[1].InnerText;
+
+                factura factura = new factura();
+
+                factura.planta = Uti.mod_planta(planta);
+                factura.unidad = Uti.mod_destino(direccion, planta, destino);
+                factura.fecha = DateTime.Parse(Uti.mod_fecha(fecha));
+                factura.codFactura = codfactura.Substring(15, 13);
+                factura.octanaje = Uti.mod_octanaje(octanaje);
+                factura.monto = Double.Parse(Uti.mod_monto(monto));
+                factura.scop = scop;
+                factura.galones = int.Parse(galones);
+
+                paquete.Add(factura);
+                
+            }
+
+            // Elimina todo el contenido en la carpeta UploadedFiles
+
+            System.IO.DirectoryInfo di = new DirectoryInfo(servidor);
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+            foreach (DirectoryInfo dir in di.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+
+            // Devuelve Array de Facturas
+
+            return paquete;
+
+        }
+
+        public void subir_facturas()
+        {
+            using (Entities obj = new Entities())
+            {
+                foreach (factura factura in paquete)
+                {
+                    obj.factura.Add(factura);
+                }
+                obj.SaveChanges();
+            }
+        }
+    }
+}
